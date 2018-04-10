632bf2507c1992129e4d905be4eddac808ac2174
Adamm91 <adam.marcinkowski91@gmail.com>
2018-04-10 18:59:55 +0200

    calculation of blank and null elements

--- src/main/java/com/sda/stringCalculator/StringCal.java
+++ src/main/java/com/sda/stringCalculator/StringCal.java
@@ -1,14 +1,22 @@
 package com.sda.stringCalculator;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class StringCal {
 
     public int calculate (String text) {
-        String[] numbers = text.split(";");
+//         if (StringUtils.isBlank(text) return 0;
+        String result = isNullOrBlank(text) ? "0" : StringUtils.deleteWhitespace(text);
+        String[] numbers = result.split(";");
         int sum = 0;
         for (String number : numbers) {
             sum += Integer.valueOf(number);
         }
         return sum;
+    }
+
+    public boolean isNullOrBlank (String text) {
+        return StringUtils.isBlank(text);
     }
 
 }
--- src/test/java/com/sda/stringCalculator/StringCalTest.java
+++ src/test/java/com/sda/stringCalculator/StringCalTest.java
@@ -21,4 +21,31 @@ public class StringCalTest {
         Assert.assertEquals(37, stringCal.calculate("21;16"));
     }
 
+    @Test
+    public void IsNullTest() {
+        Assert.assertEquals(0, stringCal.calculate(null));
+    }
+
+    @Test
+    public void isBlankTest() {
+        Assert.assertEquals(0, stringCal.calculate(""));
+    }
+
+    @Test
+    public void oneNumberTest() {
+        Assert.assertEquals(5, stringCal.calculate("5"));
+    }
+
+    @Test
+    public void multipleNumbersTest() {
+        Assert.assertEquals(24, stringCal.calculate("2;15;4;3"));
+        Assert.assertEquals(27, stringCal.calculate("4;18;5"));
+    }
+
+    @Test
+    public void isWhiteSpacesTest() {
+        Assert.assertEquals(5, stringCal.calculate("2; 3"));
+        Assert.assertEquals(12, stringCal.calculate("5; 7"));
+    }
+
 }
