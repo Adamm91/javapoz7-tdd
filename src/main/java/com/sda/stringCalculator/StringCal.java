a7dfca4e24c0804be122e6c8aa02af9e11d41bba
Adamm91 <adam.marcinkowski91@gmail.com>
2018-04-10 19:18:53 +0200

    refactor with lambda

--- src/main/java/com/sda/stringCalculator/StringCal.java
+++ src/main/java/com/sda/stringCalculator/StringCal.java
@@ -2,6 +2,8 @@ package com.sda.stringCalculator;
 
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.Arrays;
+
 public class StringCal {
 
     public int calculate (String text) {
@@ -9,11 +11,9 @@ public class StringCal {
         String withoutSpaces = StringUtils.deleteWhitespace(text);
         String[] numbers = StringUtils.split(withoutSpaces, ";");
         numbers = numbers == null ? new String[0] : numbers;
-        int sum = 0;
-        for (String number : numbers) {
-            sum += Integer.valueOf(number);
-        }
-        return sum;
+        return Arrays.stream(numbers)
+                .mapToInt(e -> Integer.valueOf(e))
+                .sum();
     }
 
 
